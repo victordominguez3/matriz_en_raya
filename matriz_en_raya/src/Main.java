d094893f45a8aa201e26b0a4ffe945725055eb6d
Víctor Domínguez <victor.dominguez@alumno.iesluisvives.org>
2022-11-24 14:36:04 +0100

    Matriz de matrices

--- matriz_en_raya/src/Main.java
+++ matriz_en_raya/src/Main.java
@@ -1,5 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner cs = new Scanner(System.in);
+
+        String[][][][] tablero = new String[3][3][3][3];
+
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                for (int k = 0; k < tablero[i][j].length; k++) {
+                    for (int l = 0; l < tablero[i][j][k].length; l++) {
+
+                        tablero[i][j][k][l] = "-";
+                        System.out.print(tablero[i][j][k][l] + "  ");
+
+                    }
+                    System.out.print("   ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
     }
+
 }
