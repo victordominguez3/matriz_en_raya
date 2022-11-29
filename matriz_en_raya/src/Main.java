8af40ab347de89f5737bae81bebd7ed35f38f909
Víctor Domínguez <victor.dominguez@alumno.iesluisvives.org>
2022-11-29 17:34:02 +0100

    Sudoku

--- matriz_en_raya/src/Main.java
+++ matriz_en_raya/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         int[][][][] sudoku = new int[3][3][3][3];
 
-        inicializarSudoku(sudoku);
+        sudoku = inicializarSudoku(sudoku);
 
     }
 
@@ -33,11 +33,11 @@ public class Main {
 
     }
 
-    public static void inicializarSudoku(int[][][][] sudoku) {
+    public static int[][][][] inicializarSudoku(int[][][][] sudoku) {
 
         Random random = new Random();
-        int num;
-        int[] posiciones = new int[4];
+        int[] numeros;
+        int pos;
 
         for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku[i].length; j++) {
@@ -51,14 +51,55 @@ public class Main {
             }
         }
 
-        do {
-            posiciones = posRandom(posiciones);
-        } while ();
+        for (int i = 0; i < sudoku.length; i++) {
+            for (int j = 0; j < sudoku[i].length; j++) {
+                numeros = inicializarNumeros();
+                for (int k = 0; k < sudoku[i][j].length; k++) {
+                    for (int l = 0; l < sudoku[i][j][k].length; l++) {
+                        do {
+                            pos = random.nextInt(numeros.length);
+                            sudoku[i][j][k][l] = numeros[pos];
+                            numeros = quitarNumero(numeros, pos);
+                        } while ();
+                    }
+                }
+            }
+        }
 
+        return sudoku;
     }
 
-    public static int[] posRandom(int[] posiciones) {
+    public static int[] inicializarNumeros() {
 
+        int[] numeros = new int[9];
+        int cont = 1;
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = cont;
+            cont++;
+        }
+
+        return numeros;
+    }
+
+    public static int[] quitarNumero(int[] numeros, int pos) {
+
+        int[] newNumeros = new int[numeros.length - 1];
+        int guardar;
+
+        for (int i = pos; i < numeros.length; i++) {
+            if (i != numeros.length - 1) {
+                guardar = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = guardar;
+            }
+        }
+
+        for (int i = 0; i < newNumeros.length; i++) {
+            newNumeros[i] = numeros[i];
+        }
+
+        return newNumeros;
     }
 
 }
